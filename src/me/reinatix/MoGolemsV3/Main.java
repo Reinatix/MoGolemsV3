417ef4396bd25083fb459308214d872d10bb79cd
Reinatix <reinatix@gmail.com>
2018-05-13 11:01:43 -0400

    Added the backend

--- src/me/reinatix/MoGolemsV3/Main.java
+++ src/me/reinatix/MoGolemsV3/Main.java
@@ -1,4 +1,29 @@
 package me.reinatix.MoGolemsV3;
 
-public class Main {
+import me.reinatix.MoGolemsV3.commands.SpawnerCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public static Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        registerCommands();
+        registerEvents();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    private void registerCommands() {
+        getCommand("spawner").setExecutor(new SpawnerCommand());
+    }
+
+    private void registerEvents() {
+
+    }
 }
---
+++ src/me/reinatix/MoGolemsV3/utils/Utils.java
@@ -0,0 +1,42 @@
+package me.reinatix.MoGolemsV3.utils;
+
+import me.reinatix.MoGolemsV3.Main;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils {
+
+    public static String colorize(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+
+    public static void send(Player player, String msg) {
+        player.sendMessage(colorize(msg));
+    }
+
+    public void giveSpawner(Player player, String tier, int amount) {
+        FileConfiguration config = Main.instance.getConfig();
+        ItemStack spawner = new ItemStack(Material.matchMaterial(config.getString("spawners." + tier + ".type")));
+        ItemMeta meta = spawner.getItemMeta();
+        meta.setDisplayName(config.getString("spawners." + tier + ".name"));
+        List<String> oldLore = config.getStringList("spawners." + tier + ".lore");
+        List<String> newLore = new ArrayList<>();
+        for (String s : oldLore) {
+            Utils.colorize(s);
+            newLore.add(s);
+        }
+        meta.setLore(newLore);
+        spawner.setAmount(amount);
+        spawner.setItemMeta(meta);
+
+        player.getInventory().addItem(spawner);
+    }
+
+}
